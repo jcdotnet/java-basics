4b5ba59c59d309306e2b6b6090412d2f41d897db
JC <jcdotnet@hotmail.com>
2025-08-23 12:48:40 +0200

    TDD(Fizz buzz) :: tests

---
+++ tdd/src/test/java/com/josecarlosroman/basics/java/tdd/fizzbuzz/FizzbuzzTest.java
@@ -0,0 +1,66 @@
+package com.josecarlosroman.basics.java.tdd.fizzbuzz;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+/*public*/ class FizzbuzzTest { // no need to be public
+
+    @Test
+    @DisplayName("First number is one")
+    @Order(1)
+    void testFirstNumberIsOne() {
+        fail("fail");
+    }
+
+    @Test
+    @DisplayName("Third number is Fizz")
+    @Order(2)
+    void testThirdNumberIsFizz() {
+        fail("fail");
+    }
+
+    @Test
+    @DisplayName("Fifth number is Buzz")
+    @Order(3)
+    void testFifthNumberIsBuzz() {
+        fail("fail");
+    }
+
+    @Test
+    @DisplayName("Fifteen number is Fizzbuzz")
+    @Order(4)
+    void testFifteenNumberIsFizzbuzz() {
+        fail("fail");
+    }
+
+    @Test
+    @DisplayName("Fizz")
+    @Order(5)
+    void testForFizz() {
+        fail("fail");
+    }
+
+    @Test
+    @DisplayName("Buzz")
+    @Order(6)
+    void testForBuzz() {
+        fail("fail");
+    }
+
+    @Test
+    @DisplayName("Fizzbuzz")
+    @Order(7)
+    void testForFizzbuzz() {
+        fail("fail");
+    }
+
+    @Test
+    @DisplayName("getNumbers must return 100 numbers")
+    @Order(8)
+    void testGetNumbersMustReturn100Numbers() {
+        fail("fail");
+    }
+
+}
